382ccf0a4a15614eb2e414e3c59ca4a404aa3c1a
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-20 12:05:50 -0300

    Aula do dia 20/09

---
+++ 1 Semestre/Aula2009/app/src/main/java/com/example/aulafragments/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.aulafragments
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnCadastro:Button
+    private lateinit var btnLogin:Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        btnCadastro = findViewById(R.id.btnCadastro)
+
+        btnCadastro.setOnClickListener{
+            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView,CadastroFragment()).commit()
+        }
+
+        btnLogin = findViewById(R.id.btnLogin)
+
+        btnLogin.setOnClickListener{
+            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView,LoginFragment()).commit()
+        }
+    }
+}

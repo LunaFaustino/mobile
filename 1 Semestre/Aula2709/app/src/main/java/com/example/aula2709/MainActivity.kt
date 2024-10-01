ee51f024aa342c6ff9422610b4980625c5d57336
Luna Faustino <lu.faustino2000@gmail.com>
2024-10-01 09:49:30 -0300

    Continuação aula 27/09

--- 1 Semestre/Aula2709/app/src/main/java/com/example/aula2709/MainActivity.kt
+++ 1 Semestre/Aula2709/app/src/main/java/com/example/aula2709/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.aula2709
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,6 +16,11 @@ class MainActivity : AppCompatActivity() {
     private val bancoDados by lazy {
         DatabaseHelper(this)
     }
+
+    private lateinit var editNomeProduto:EditText
+    private lateinit var btnSalvar:Button
+    private lateinit var btnListar:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -24,12 +31,39 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        editNomeProduto = findViewById(R.id.editNomeProduto)
+        btnSalvar = findViewById(R.id.btnSalvar)
+        btnListar = findViewById(R.id.btnListar)
+
+        btnSalvar.setOnClickListener {
+            salvar()
+        }
+
+        btnListar.setOnClickListener {
+            listar()
+        }
+
+    }
+
+    private fun salvar(){
+        val nomeProduto = editNomeProduto.text.toString()
         try {
-            bancoDados.writableDatabase.execSQL("insert into produtos values(null, 'Iphone 11', '120GB')")
+            bancoDados.writableDatabase.execSQL("insert into produtos values(null, '$nomeProduto', '120GB')")
             Log.i("db_info","Produto inserido com sucesso")
         }catch (e:Exception){
             e.printStackTrace()
         }
+    }
 
+    private fun listar(){
+        val sql = "select * from produtos"
+        val cursor = bancoDados.readableDatabase.rawQuery(sql,null)
+
+        while (cursor.moveToNext()){
+            val nome = cursor.getString(1)
+            val descricao = cursor.getString(2)
+            Log.i("db_info","Nome: $nome Descrição: $descricao")
+
+        }
     }
 }
